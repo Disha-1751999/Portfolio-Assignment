81144a9e6c3b7db35bfdc8e16ed79bbbbeae587d
Disha-1751999 <80054827+Disha-1751999@users.noreply.github.com>
2025-02-06 13:20:30 +0600

    add

--- client/src/skeleton/TableSkeleton.jsx
+++ client/src/skeleton/TableSkeleton.jsx
@@ -1,50 +1,33 @@
-import React from 'react'
+import React from 'react';
 import Skeleton from "react-loading-skeleton";
+import "react-loading-skeleton/dist/skeleton.css"; // Ensure Skeleton styles are included
 
 function TableSkeleton() {
   return (
-    
     <table className="table table-hover mb-5 w-100 my-5">
-      <thead className="table-dark"> 
+      <thead className="table-dark">
         <tr>
-          <th scope="col"><Skeleton count={1} /></th>
-          <th scope="col"><Skeleton count={1} /></th>
-          <th scope="col"><Skeleton count={1} /></th>
+          <th scope="col"><Skeleton width="100%" height={25} /></th>
+          <th scope="col"><Skeleton width="100%" height={25} /></th>
+          <th scope="col"><Skeleton width="100%" height={25} /></th>
         </tr>
       </thead>
       <tbody>
-
-      {
-                                   Array.from({length:16}).map((item,index)=>{
-                                       return(
-
-                                        <tr key={index}>
-              <td>  <Skeleton count={1} /></td>
-              <td>  <Skeleton count={1} /></td>
-              <td>
-                <div className="d-flex justify-content-start gap-2 flex-wrap">
-                  <button
-                  >
-                      <Skeleton count={1} />
-                  </button>
-                  <button
-                    
-                  >
-                      <Skeleton count={1} />
-                  </button>
-                </div>
-              </td>
-            </tr>
-                                         
-                                       )
-                                   })
-                               }
-            
-        
+        {Array.from({ length: 16 }).map((_, index) => (
+          <tr key={index}>
+            <td><Skeleton width="100%" height={20} /></td>
+            <td><Skeleton width="100%" height={20} /></td>
+            <td>
+              <div className="d-flex justify-content-start gap-2 flex-wrap">
+                <Skeleton width={80} height={30} /> {/* Simulating button */}
+                <Skeleton width={80} height={30} /> {/* Simulating button */}
+              </div>
+            </td>
+          </tr>
+        ))}
       </tbody>
     </table>
-  
-  )
+  );
 }
 
-export default TableSkeleton
+export default TableSkeleton;
